ce057396af1dac471c98f68f9f09ce87f190b1ba
Yoseph Zemede <yoseph_zemede@yahoo.com>
2022-12-15 16:50:51 -0800

    Finished all the tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests functions that checkes if number is postive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+
+	postive_or_negative(i);
+
+	return (0);
+}
